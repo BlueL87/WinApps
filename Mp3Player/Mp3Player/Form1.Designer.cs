5119a6a4d268d31bffe23197ed1bb167975e4e5a
Steve <s87117@gmail.com>
2017-06-18 13:45:13 +1200

    Update 1

--- Mp3Player/Mp3Player/Form1.Designer.cs
+++ Mp3Player/Mp3Player/Form1.Designer.cs
@@ -28,12 +28,216 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fILEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aBOUTToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vERSIONToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aUTHORToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnUp = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.lbVolume = new System.Windows.Forms.Label();
+            this.lbTitle = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fILEToolStripMenuItem,
+            this.aBOUTToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(615, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fILEToolStripMenuItem
+            // 
+            this.fILEToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFileToolStripMenuItem});
+            this.fILEToolStripMenuItem.Name = "fILEToolStripMenuItem";
+            this.fILEToolStripMenuItem.Size = new System.Drawing.Size(47, 24);
+            this.fILEToolStripMenuItem.Text = "FILE";
+            // 
+            // openFileToolStripMenuItem
+            // 
+            this.openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
+            this.openFileToolStripMenuItem.Size = new System.Drawing.Size(122, 26);
+            this.openFileToolStripMenuItem.Text = "OPEN";
+            this.openFileToolStripMenuItem.Click += new System.EventHandler(this.openFileToolStripMenuItem_Click);
+            // 
+            // aBOUTToolStripMenuItem
+            // 
+            this.aBOUTToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.vERSIONToolStripMenuItem,
+            this.aUTHORToolStripMenuItem});
+            this.aBOUTToolStripMenuItem.Name = "aBOUTToolStripMenuItem";
+            this.aBOUTToolStripMenuItem.Size = new System.Drawing.Size(69, 24);
+            this.aBOUTToolStripMenuItem.Text = "ABOUT";
+            // 
+            // vERSIONToolStripMenuItem
+            // 
+            this.vERSIONToolStripMenuItem.Name = "vERSIONToolStripMenuItem";
+            this.vERSIONToolStripMenuItem.Size = new System.Drawing.Size(144, 26);
+            this.vERSIONToolStripMenuItem.Text = "VERSION";
+            this.vERSIONToolStripMenuItem.Click += new System.EventHandler(this.vERSIONToolStripMenuItem_Click);
+            // 
+            // aUTHORToolStripMenuItem
+            // 
+            this.aUTHORToolStripMenuItem.Name = "aUTHORToolStripMenuItem";
+            this.aUTHORToolStripMenuItem.Size = new System.Drawing.Size(144, 26);
+            this.aUTHORToolStripMenuItem.Text = "AUTHOR";
+            this.aUTHORToolStripMenuItem.Click += new System.EventHandler(this.aUTHORToolStripMenuItem_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.Transparent;
+            this.btnStop.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnStop.FlatAppearance.BorderSize = 0;
+            this.btnStop.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnStop.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.ForeColor = System.Drawing.Color.Aqua;
+            this.btnStop.Location = new System.Drawing.Point(149, 157);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(88, 33);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "STOP";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.BackColor = System.Drawing.Color.Transparent;
+            this.btnPlay.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPlay.FlatAppearance.BorderSize = 0;
+            this.btnPlay.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnPlay.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlay.ForeColor = System.Drawing.Color.Crimson;
+            this.btnPlay.Location = new System.Drawing.Point(356, 157);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(88, 33);
+            this.btnPlay.TabIndex = 2;
+            this.btnPlay.Text = "PLAY";
+            this.btnPlay.UseVisualStyleBackColor = false;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.BackColor = System.Drawing.Color.Transparent;
+            this.btnPause.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPause.FlatAppearance.BorderSize = 0;
+            this.btnPause.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnPause.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPause.ForeColor = System.Drawing.Color.Gold;
+            this.btnPause.Location = new System.Drawing.Point(243, 157);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(107, 33);
+            this.btnPause.TabIndex = 3;
+            this.btnPause.Text = "PAUSE";
+            this.btnPause.UseVisualStyleBackColor = false;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnUp
+            // 
+            this.btnUp.BackColor = System.Drawing.Color.Transparent;
+            this.btnUp.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnUp.FlatAppearance.BorderSize = 0;
+            this.btnUp.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnUp.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUp.ForeColor = System.Drawing.Color.Crimson;
+            this.btnUp.Location = new System.Drawing.Point(361, 304);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(103, 33);
+            this.btnUp.TabIndex = 4;
+            this.btnUp.Text = "UP";
+            this.btnUp.UseVisualStyleBackColor = false;
+            this.btnUp.Click += new System.EventHandler(this.btnUp_Click);
+            // 
+            // btnDown
+            // 
+            this.btnDown.BackColor = System.Drawing.Color.Transparent;
+            this.btnDown.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDown.FlatAppearance.BorderSize = 0;
+            this.btnDown.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnDown.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDown.ForeColor = System.Drawing.Color.Crimson;
+            this.btnDown.Location = new System.Drawing.Point(361, 343);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(103, 33);
+            this.btnDown.TabIndex = 5;
+            this.btnDown.Text = "DOWN";
+            this.btnDown.UseVisualStyleBackColor = false;
+            this.btnDown.Click += new System.EventHandler(this.btnDown_Click);
+            // 
+            // lbVolume
+            // 
+            this.lbVolume.BackColor = System.Drawing.Color.Transparent;
+            this.lbVolume.Font = new System.Drawing.Font("Times New Roman", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbVolume.ForeColor = System.Drawing.Color.Aqua;
+            this.lbVolume.Location = new System.Drawing.Point(282, 304);
+            this.lbVolume.Name = "lbVolume";
+            this.lbVolume.Size = new System.Drawing.Size(73, 72);
+            this.lbVolume.TabIndex = 6;
+            this.lbVolume.Text = "70";
+            this.lbVolume.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbTitle
+            // 
+            this.lbTitle.BackColor = System.Drawing.Color.Transparent;
+            this.lbTitle.Font = new System.Drawing.Font("Times New Roman", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTitle.ForeColor = System.Drawing.Color.Gold;
+            this.lbTitle.Location = new System.Drawing.Point(142, 64);
+            this.lbTitle.Name = "lbTitle";
+            this.lbTitle.Size = new System.Drawing.Size(302, 60);
+            this.lbTitle.TabIndex = 7;
+            this.lbTitle.Text = "Music Player";
+            this.lbTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Mp3Player.Properties.Resources.bg;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(615, 497);
+            this.Controls.Add(this.lbTitle);
+            this.Controls.Add(this.lbVolume);
+            this.Controls.Add(this.btnDown);
+            this.Controls.Add(this.btnUp);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fILEToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aBOUTToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vERSIONToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aUTHORToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFileToolStripMenuItem;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.Label lbVolume;
+        private System.Windows.Forms.Label lbTitle;
     }
 }
 
--- Mp3Player/Mp3Player/Form1.cs
+++ Mp3Player/Mp3Player/Form1.cs
@@ -12,9 +12,114 @@ namespace Mp3Player
 {
     public partial class Form1 : Form
     {
+        private WMPLib.WindowsMediaPlayer wplayer;
+
         public Form1()
         {
             InitializeComponent();
+            InitPlayer();
+        }
+
+        private void InitPlayer()
+        {
+            wplayer = new WMPLib.WindowsMediaPlayer();
+            setVolume(70);
+        }
+
+        private void aUTHORToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string message = "It is a free mp3 player made by Steve";
+            string caption = "Author";
+            MessageBoxButtons buttons = MessageBoxButtons.OK;
+            DialogResult result = MessageBox.Show(message, caption, buttons);
+        }
+
+        private void vERSIONToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string message = "Version  Luna-1.0.0";
+            string caption = "Version";
+            MessageBoxButtons buttons = MessageBoxButtons.OK;
+            DialogResult result = MessageBox.Show(message, caption, buttons);
+        }
+
+        private void openFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            InitMusicSelectionDlg();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (wplayer != null)
+                wplayer.controls.stop();
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            if (wplayer != null)
+                wplayer.controls.pause();
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            if (wplayer.URL == null || wplayer.URL == "")
+                InitMusicSelectionDlg();
+            else
+                wplayer.controls.play();
+
+        }
+
+        private void InitMusicSelectionDlg()
+        {
+            System.IO.Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "mp3 files (*.mp3)|*.mp3|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            wplayer.URL = openFileDialog1.FileName;
+                            wplayer.controls.play();
+                            setTitle("");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not play the file. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void btnUp_Click(object sender, EventArgs e)
+        {
+            if (wplayer != null && wplayer.settings.volume < 100)
+                setVolume(wplayer.settings.volume+5);
+        }
+
+        private void btnDown_Click(object sender, EventArgs e)
+        {
+            if (wplayer != null && wplayer.settings.volume > 1)
+                setVolume(wplayer.settings.volume-5);
+        }
+
+        private void setVolume(int value)
+        {
+            wplayer.settings.volume = value;
+            lbVolume.Text = wplayer.settings.volume.ToString();
+        }
+
+        private void setTitle(string title)
+        {
+            lbTitle.Text = title;
         }
     }
 }
